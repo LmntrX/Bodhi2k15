1cea50f1a274864ce15bf7642f3b90e6d9ea9323
livin21 <livinmathew99@gmail.com>
2015-09-19 18:59:09 +0530

    Bodhi2k15

---
+++ app/src/main/java/com/lmntrx/bodhioffline/MainActivity.java
@@ -0,0 +1,82 @@
+package com.lmntrx.bodhioffline;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+
+    //WebView
+    private WebView mWebview ;
+
+    //Site Url
+    String fileURL="file:///android_asset/index";
+    String mURL="http://bodhiofficial.in/";
+
+    //Context Variable
+    public static Context CON;
+
+    boolean isConnected;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mWebview  = new WebView(this);
+
+        CON=this;
+
+        //Executing Async task
+        Thread thread=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    isConnected=new NetworkStatusCheck().execute().get();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.run();
+
+        mWebview.getSettings().setJavaScriptEnabled(true); // enable javascript
+        WebSettings webSettings = mWebview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
+            webSettings.setAllowFileAccessFromFileURLs(true);
+            webSettings.setAllowUniversalAccessFromFileURLs(true);
+        }
+
+        final Activity activity = this;
+
+        mWebview.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(activity, description, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        if (isConnected){
+            mWebview.loadUrl(mURL);
+            Toast.makeText(CON,"App Powered By LmntrX\n    App in Online Mode",Toast.LENGTH_LONG).show();
+        }
+        else{
+            mWebview.loadUrl(fileURL);
+            Toast.makeText(CON,"App Powered By LmntrX\n    App in Offline Mode",Toast.LENGTH_LONG).show();
+        }
+
+        setContentView(mWebview);
+
+
+
+    }
+}
---
+++ app/src/main/java/com/lmntrx/bodhioffline/NetworkStatusCheck.java
@@ -0,0 +1,49 @@
+package com.lmntrx.bodhioffline;
+
+/**
+ * Created by livin on 19/9/15.
+ */
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.os.AsyncTask;
+import android.util.Log;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class NetworkStatusCheck extends AsyncTask<Void, Void, Boolean> {
+
+    //Self Documented :P
+    boolean result=false;
+    Context CON;
+
+    @Override
+    protected Boolean doInBackground(Void... params) {
+        CON=MainActivity.CON;
+
+        if (isNetworkAvailable(CON)) {
+            try {
+                HttpURLConnection urlc = (HttpURLConnection) (
+                        new URL("http://www.lmntrx.com").openConnection()); //LmntrX :D
+                urlc.setRequestProperty("User-Agent", "Test");
+                urlc.setRequestProperty("Connection", "close");
+                urlc.setConnectTimeout(1500);
+                urlc.connect();
+                result = (urlc.getResponseCode() == 200);
+            } catch (IOException e) {
+                Log.e("No Connection", "Error checking internet connection", e);
+                result=false;
+            }
+        } else {
+            result = false;
+        }
+
+        return result;
+    }
+
+    public boolean isNetworkAvailable(final Context context) {
+        final ConnectivityManager connectivityManager = ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE));
+        return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
+    }
+}
