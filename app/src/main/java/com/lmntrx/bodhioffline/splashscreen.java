05ecec8cadee9765fab7d658f7ef5f09fa2c7e43
livin21 <livinmathew99@gmail.com>
2015-09-21 11:05:11 +0530

    Parse Analytics Added+Fixed

--- app/src/main/java/com/lmntrx/bodhioffline/splashscreen.java
+++ app/src/main/java/com/lmntrx/bodhioffline/splashscreen.java
@@ -31,8 +31,6 @@ public class splashscreen extends Activity {
         Con=this;
         splash=this;
 
-        // declare this before onCreate:     private RelativeLayout splashLayout;
-
         splashLayout=(RelativeLayout)findViewById(R.id.splash);
 
         final Handler handler = new Handler();
